03dd82d632fe045f9b916abf54b55322618f772d
syjo <Whtjd6475^>
2018-12-05 16:24:24 +0900

    (2018년)KAKAO BLIND RECRUITMENT "후보키" 문제 최초 커밋

---
+++ src/main/java/kakao/CandidateKey.java
@@ -0,0 +1,15 @@
+package kakao;
+
+import java.util.stream.*;
+
+/**
+ * 후보키
+ * Created by sungyeon on 05/12/2018.
+ */
+public class CandidateKey {
+    public int solution(String[][] relation) {
+        int answer = 0;
+
+        return answer;
+    }
+}
---
+++ src/test/java/kakao/CandidateKeyTest.java
@@ -0,0 +1,28 @@
+package kakao;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by sungyeon on 05/12/2018.
+ */
+public class CandidateKeyTest {
+    CandidateKey sut = new CandidateKey();
+
+    @Test
+    public void testResult() {
+        String[][] relation = {
+                {"100","ryan","music","2"},
+                {"200","apeach","math","2"},
+                {"300","tube","computer","3"},
+                {"400","con","computer","4"},
+                {"500","muzi","music","3"},
+                {"600","apeach","music","2"}
+        };
+
+        int actual = sut.solution(relation);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+}
