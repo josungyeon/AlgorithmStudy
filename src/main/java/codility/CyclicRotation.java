38d1fada59dc8e611fb4a8e317287be0955d4bb7
sungyeonjo <jsy6285@naver.com>
2019-08-03 21:59:42 +0900

    codility

--- src/main/java/codility/CyclicRotation.java
+++ src/main/java/codility/CyclicRotation.java
@@ -6,41 +6,36 @@ import java.util.*;
  * Created by sungyeonjo on 2019. 4. 13..
  */
 public class CyclicRotation {
-    public String solution(int[] T) {
-        int month = T.length / 4;
+    // * 2번
+    public static String solution(int[] T) {
+        int monthsPerSeason = T.length / 4;
         List<Integer> winter = new ArrayList<>();
         List<Integer> spring = new ArrayList<>();
         List<Integer> summer = new ArrayList<>();
         List<Integer> autumn = new ArrayList<>();
 
-        for (int i = 0; i < month; i++) {
+        for (int i = 0; i < monthsPerSeason; i++) {
             winter.add(T[i]);
-            spring.add(T[i+month]);
-            summer.add(T[i+month*2]);
-            autumn.add(T[i+month*3]);
+            spring.add(T[i + monthsPerSeason]);
+            summer.add(T[i + monthsPerSeason * 2]);
+            autumn.add(T[i + monthsPerSeason * 3]);
         }
 
-        Map<String, Integer> fourSeasonsMap = new HashMap<>();
-        fourSeasonsMap.put("WINTER", Collections.max(winter) - Collections.min(winter));
-        fourSeasonsMap.put("SPRING", Collections.max(spring) - Collections.min(spring));
-        fourSeasonsMap.put("SUMMER", Collections.max(summer) - Collections.min(summer));
-        fourSeasonsMap.put("AUTUMN", Collections.max(autumn) - Collections.min(autumn));
+        Map<String, Integer> fourSeasonsTemperatureGap = new HashMap<>();
+        fourSeasonsTemperatureGap.put("WINTER", Collections.max(winter) - Collections.min(winter));
+        fourSeasonsTemperatureGap.put("SPRING", Collections.max(spring) - Collections.min(spring));
+        fourSeasonsTemperatureGap.put("SUMMER", Collections.max(summer) - Collections.min(summer));
+        fourSeasonsTemperatureGap.put("AUTUMN", Collections.max(autumn) - Collections.min(autumn));
 
-        int max = Collections.max(winter) - Collections.min(winter);
         String season = "";
-
-        for (Map.Entry map: fourSeasonsMap.entrySet()) {
-            if (max < (int)map.getValue()) {
-                max = (int)map.getValue();
-                season = (String)map.getKey();
+        int maxTempGap = 0;
+        for (Map.Entry<String, Integer> map: fourSeasonsTemperatureGap.entrySet()) {
+            if (maxTempGap <= map.getValue()) {
+                maxTempGap = map.getValue();
+                season = map.getKey();
             }
         }
-
         return season;
     }
 
-    public static void main(String[] args) {
-        CyclicRotation sut = new CyclicRotation();
-        sut.solution(new int[]{-3, -14, -5, 7, 8, 42, 8, 2});
-    }
 }
--- src/test/java/codility/PleaseLastTest.java
+++ src/test/java/codility/PleaseLastTest.java
@@ -8,25 +8,45 @@ import static org.junit.Assert.*;
  */
 public class PleaseLastTest {
     @Test
-    public void test() {
-        assertEquals(PleaseLast.solution(new int[]{3, 5, 6, 3, 3, 5}), 4);
-    }
-
-    @Test
-    public void test2() {
-        assertEquals(PleaseLast.solution2(new int[]{3,4,3,0,2,2,3,0,0}), 5);
-        assertEquals(PleaseLast.solution2(new int[]{4,4,3,3,1,0}), 3);
-        assertEquals(PleaseLast.solution2(new int[]{4, 2, 0}), 0);
+    public void test1() {
+        assertEquals(ReTestNum1.solution3(83643), "23h14m3s");
+        assertEquals(ReTestNum1.solution3(7500), "2h5m0s");
+        assertEquals(ReTestNum1.solution3(0), "0h0m0s");
+        assertEquals(ReTestNum1.solution3(86399), "23h59m59s");
     }
 
     @Test
     public void test3() {
-        assertEquals(PleaseLast.solution3(83643), "23h14m3s");
-        assertEquals(PleaseLast.solution3(7500), "2h5m0s");
+        assertEquals(ReTestNum3.solution(new int[]{3,4,3,0,2,2,3,0,0}), 5);
+        assertEquals(ReTestNum3.solution(new int[]{4,4,3,3,1,0}), 3);
+        assertEquals(ReTestNum3.solution(new int[]{4, 2, 0}), 0);
+        assertEquals(ReTestNum3.solution(new int[]{1,2}), 1);
+        assertEquals(ReTestNum3.solution(new int[]{0,0,0,3,4,5,6}), 3);
+        int[] array = new int[100000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        assertEquals(ReTestNum3.solution(array), 99999);
     }
 
     @Test
     public void test4() {
+        assertEquals(ReTestNum4.solution(new int[]{}), 0);
+        assertEquals(ReTestNum4.solution(new int[]{1, 1}), 1);
+        assertEquals(ReTestNum4.solution(new int[]{-1000000000, -1000000000, -100000000, 1000000000}), 1);
+        assertEquals(ReTestNum4.solution(new int[]{1000000000, 1000000000, 1000000000, 1000000000}), 6);
+        assertEquals(ReTestNum4.solution(new int[]{3, 5, 6, 3, 3, 5}), 4);
+        int[] array = new int[1000000];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 1000;
+        }
+        assertEquals(ReTestNum4.solution(array), 1000000000);
+    }
+
+    @Test
+    public void test() {
         assertEquals(PleaseLast.solution4("011100"), 7);
+        assertEquals(11, PleaseLast.solution4("10000000000"));
+        assertEquals(0, PleaseLast.solution4("0"));
     }
 }
